a103ca9e618ed0502e5057b70ad6bb100cd0d838
Jacob Reid <reidwjacob@gmail.com>
2015-06-27 16:56:54 -0600

    First attempt at asking roots for data using progress component

--- blossom/components/Progress.php
+++ blossom/components/Progress.php
@@ -1,6 +1,23 @@
 <?php namespace Delphinium\Blossom\Components;
 
+use Delphinium\Roots\UpdatableObjects\Module;
+use Delphinium\Roots\UpdatableObjects\ModuleItem;
+use Delphinium\Roots\Models\Assignment;
+use Delphinium\Roots\Models\ModuleItem as DbModuleItem;
+use Delphinium\Roots\Roots;
+use Delphinium\Roots\RequestObjects\SubmissionsRequest;
+use Delphinium\Roots\RequestObjects\ModulesRequest;
+use Delphinium\Roots\RequestObjects\AssignmentsRequest;
+use Delphinium\Roots\RequestObjects\AssignmentGroupsRequest;
+use Delphinium\Roots\Enums\CommonEnums\ActionType;
+use Delphinium\Roots\Enums\ModuleItemEnums\ModuleItemType;
+use Delphinium\Roots\Enums\ModuleItemEnums\CompletionRequirementType;
 use Cms\Classes\ComponentBase;
+use \DateTime;
+use GuzzleHttp\Client;
+use GuzzleHttp\Post\PostFile;
+
+use Delphinium\Roots\Guzzle\GuzzleHelper;
 
 class Progress extends ComponentBase
 {
@@ -23,5 +40,12 @@ class Progress extends ComponentBase
         $this->addJs("/plugins/delphinium/blossom/assets/javascript/d3.min.js");
         $this->addCss("/plugins/delphinium/blossom/assets/css/main.css");
         $this->addCss("/plugins/delphinium/blossom/assets/css/progress.css");
+        
+        $req = new AssignmentsRequest(ActionType::GET);
+        
+        $roots = new Roots();
+        $res = $roots->assignments($req);
+        $this->page['assignments'] = json_encode($res);
     }
+
 }
