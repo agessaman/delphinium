7b6011c6d59b0015f10089583e69987899a50ec1
Daniel Clark <danielsan88@gmail.com>
2015-08-21 12:53:23 -0600

    Removed template comments.

--- Blade/Classes/Rules/Operator/Tautology.php
+++ Blade/Classes/Rules/Operator/Tautology.php
@@ -1,18 +1,10 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Delphinium\Blade\Classes\Rules\Operator;
 use Delphinium\Blade\Classes\Rules\Proposition;
 
 /**
- * Description of Tautology
- *
- * @author Daniel
+ * @author Daniel Clark
  */
 class Tautology extends Operator implements Proposition {
     public function evaluate(\Delphinium\Blade\Classes\Rules\IContext $context) {
