eea749c4d69741dcc01b0b598656fdf6e9a50fda
japomani <jared.chap@gmail.com>
2015-08-25 12:59:37 -0600

    Fixed orchid

--- orchid/updates/create_quizlesson_tables.php
+++ orchid/updates/create_quizlesson_tables.php
@@ -3,12 +3,12 @@
 use Schema;
 use October\Rain\Database\Updates\Migration;
 
-class CreateQuizlessonTable extends Migration
+class CreateQuizlessonsTable extends Migration
 {
 
     public function up()
     {
-        Schema::create('delphinium_orchid_quizlesson', function($table)
+        Schema::create('delphinium_orchid_quizlessons', function($table)
         {
             $table->engine = 'InnoDB';
             $table->increments('id');
@@ -18,7 +18,7 @@ class CreateQuizlessonTable extends Migration
 
     public function down()
     {
-        Schema::dropIfExists('delphinium_orchid_quizlesson');
+        Schema::dropIfExists('delphinium_orchid_quizlessons');
     }
 
 }
