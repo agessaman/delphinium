73763edcad12339307032966786e2edd0a3ada87
damarisz <damariszg@gmail.com>
2016-01-13 14:48:23 -0700

    Update drop_grades_table.php
    Fixed error in this class --duplicate function name

--- blossom/updates/drop_grades_table.php
+++ blossom/updates/drop_grades_table.php
@@ -3,7 +3,7 @@
 use Schema;
 use October\Rain\Database\Updates\Migration;
 
-class CreateGradesTable extends Migration
+class DropGradesTable extends Migration
 {
 
     public function up()
@@ -11,4 +11,18 @@ class CreateGradesTable extends Migration
         Schema::dropIfExists('delphinium_blossom_grades');
     }
 
+    public functio down()
+    {
+        if ( !Schema::hasTable('delphinium_blossom_grades') )
+        {
+            Schema::create('delphinium_blossom_grades', function($table)
+            {
+                $table->engine = 'InnoDB';
+                $table->increments('id');
+                $table->string('Name');
+                $table->string('Size');
+                $table->timestamps();
+            });
+        }
+    }
 }
