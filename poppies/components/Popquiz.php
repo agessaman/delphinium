2cc260afb6d15b90cd83f4df483c900f004dec01
Hermes-888 <tjones@uvu.edu>
2016-03-22 22:50:55 -0600

    Poppies galore

--- poppies/components/Popquiz.php
+++ poppies/components/Popquiz.php
@@ -7,6 +7,7 @@ use Delphinium\Roots\Models\Quizquestion;
 use Delphinium\Roots\Requestobjects\QuizRequest;
 use Delphinium\Roots\Models\Assignment;
 use Delphinium\Roots\Requestobjects\AssignmentsRequest;// Add Update?
+use Delphinium\Poppies\Models\Popquiz as popquizModel;
 use \DateTime;
 
 class Popquiz extends ComponentBase
@@ -76,13 +77,93 @@ class Popquiz extends ComponentBase
 	{
 		try
         {
-			$this->addCss("/plugins/delphinium/poppies/assets/css/bootstrap.min.css");
+			if (!isset($_SESSION)) { session_start(); }
+
+            $courseID = $_SESSION['courseID'];
+            // if instance has been set
+            if( $this->property('instance') )
+            {
+                //use the instance set in CMS dropdown
+                $config = popquizModel::find($this->property('instance'));
+                $config->course_id = $_SESSION['courseID'];//$course->id;
+                $config->save();//update original record now in case it did not have course
+
+            } else {
+                // if copy has a name.
+				// note: it will after the first dynamic is created
+                $copyLength = strlen($this->property('copy_id'));
+                if($copyLength > 0 )
+                {
+                    // find all matching course 
+                    $instances = popquizModel::where('course_id','=', $courseID)->get();
+                    $instCount = count($instances);
+                    if($instCount == 0) { 
+						// none found so set to catch condition for dynamic
+                        $copyLength = 0;
+                    } else {
+                        // find instance with copy
+                        $flag=false;
+                        foreach ($instances as $instance)
+                        {
+                           if($instance->copy_id == $this->property('copy_id') )
+                           {
+                               $config = $instance;
+                               $flag=true;
+                               break;// got first one found
+                           }
+                        }
+                        //yes found courses but not matching copy. use the first one found with course id
+                        if( !$flag ) { $config = $instances[0]; }
+                    }
+                }
+                // no match found so create new dynamic instance
+                if($copyLength == 0 )
+                {
+                    $config = new popquizModel;// db record
+                    $config->name = 'dynamic_';//+ total records count?
+                    // add your fields
+                    //$config->quiz_name = '';
+					//quiz_description,game_style,questions
+                    $config->course_id = $_SESSION['courseID'];
+                    $config->copy_id = $this->property('copy_id');
+                    $config->save();// save the new record
+                }
+            }
+			// use the record in the component and frontend form 
+            $this->page['config'] = json_encode($config);
+            
+			/** get roles, a comma delimited string
+			 * check if Student
+			 * if not then set to Instructor. disregard any other roles?
+			 * role is used to determine functions and display options
+			 */
+            $role = $_SESSION['roles'];
+			
+            if(stristr($role, 'Learner')) {
+                $role = 'Learner';
+            } else { 
+                $role = 'Instructor';
+            }
+            $this->page['role'] = $role;// either Learner or Instructor
+			
             $this->addCss("/plugins/delphinium/poppies/assets/css/popquiz.css");
 			
-			$role = $this->setup();
-			$this->page['role'] = $role;// either Learner or Instructor
 			if($role=='Instructor')
 			{
+				// Build a back-end form with the context of 'frontend'
+				$formController = new \Delphinium\Poppies\Controllers\Popquiz();
+				$formController->create('frontend');
+				
+                //this is the primary key of the record you want to update
+                $this->page['recordId'] = $config->id;
+				// Append the formController to the page
+				$this->page['form'] = $formController;
+                
+                // Append Instructions page
+                $instructions = $formController->makePartial('instructions');
+				
+				// instructor code component specific
+                $this->page['instructions'] = $instructions;
 				$quizList = $this->getAllQuizzes();// choose quiz questions to use
 				$this->page['quizList'] = $quizList;
 			}
@@ -111,30 +192,7 @@ class Popquiz extends ComponentBase
             return \Response::make($this->controller->run('error'), 500);
         }
 	}
-	
-	/*
-		common functions:
-			define Role: either Instructor or Learner
-			dynamic instance:
-			pass back grade for assignment
-	*/
-	public function setup()
-	{
-		if (!isset($_SESSION)) { session_start(); }
-    
-        //$courseID = $_SESSION['courseID'];// store if needed
-		// dynamic instance? not set up yet...
-		
-		// comma delimited string
-        $roleStr = $_SESSION['roles'];
-        if(stristr($roleStr, 'Learner')) {
-            $roleStr = 'Learner';
-        } else { 
-            $roleStr = 'Instructor';
-        }
-		return $roleStr;
-	}
-	
+
 	/*
 		Instructor can choose questions from multiple quizzes
 	*/
@@ -156,6 +214,7 @@ class Popquiz extends ComponentBase
 		How can we get (this) assignment object
 		Need Intro text and points to display in game
 			if not done as a Canvas Assignment
+	UNUSED SO FAR
 	*/
 	public function getSingleAssignment()
     {
@@ -168,4 +227,49 @@ class Popquiz extends ComponentBase
         echo json_encode($res);
     }
 
+	/**
+	*  https://octobercms.com/docs/plugin/components#dropdown-properties
+	*  The method should have a name in the following format: get*Property*Options()
+	*  where Property is the property name
+	*  Fills the Configuration [dropdown] in CMS
+	*/
+    public function getInstanceOptions()
+    {
+		$instances = popquizModel::all();// records
+        $array_dropdown = ['0'=>'- select Instance - '];//id, text in dropdown
+		// populate CMS dropdown
+        foreach ($instances as $instance)
+        {
+            $array_dropdown[$instance->id] = $instance->Name;
+        }
+        return $array_dropdown;
+    }
+    
+    /**Added
+	*  frontend update component submit button
+	*  save to database and return updated record
+    *
+    *  id can be disabled in fields.yaml
+    *  id, course & copy can also be hidden
+    *  $data gets .id from config.id instructor.htm
+    *  called from instructor.htm configure settings modal
+	*/
+	public function onUpdate()
+    {
+        $data = post('Popquiz');//component name
+        $did = intval($data['id']);// convert string to integer
+        $config = popquizModel::find($did);// retrieve existing record
+        $config->quiz_name = $data['quiz_name'];// change to new data
+        //echo json_encode($config);//($data);// testing
+        
+		// add your fields to update
+        //$config->quiz_description=$data['quiz_name'];
+		//$config->game_style=$data['game_style'];
+		//$config->questions=$data['questions'];
+
+		$config->course_id = $data['course_id'];//hidden in frontend
+        $config->copy_id = $data['copy_id'];//hidden
+		$config->save();// update original record 
+		return json_encode($config);// back to instructor view
+    }
 }
---
+++ poppies/updates/update_delphinium_poppies_popquizzes.php
@@ -0,0 +1,29 @@
+<?php namespace Delphinium\Poppies\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class UpdateDelphiniumPoppiesPopquizzes extends Migration
+{
+    public function up()
+    {
+        Schema::table('delphinium_poppies_popquizzes', function($table)
+        {
+			$table->string('questions',1024);//could be long
+			$table->string('game_style',255);
+			$table->integer('course_id')->nullable();
+            $table->string('copy_id',255);
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('delphinium_poppies_popquizzes', function($table)
+        {
+			$table->dropColumn('questions');
+			$table->dropColumn('game_style');
+			$table->dropColumn('course_id');
+            $table->dropColumn('copy_id');
+        });
+    }
+}
