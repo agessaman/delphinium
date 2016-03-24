59f937253c7c33026a9998bc9b58a7d4aea14b46
Business <Business>
2016-03-24 17:27:20 -0600

    Fixed bug in iris

--- iris/components/Iris.php
+++ iris/components/Iris.php
@@ -47,9 +47,6 @@ class Iris extends ComponentBase
 
             $roots = new Roots();
             $moduleData = $roots->modules($req);
-//        echo "hello";
-//        return;
-//        echo json_encode($moduleData);return;
 
             $this->page['rawData'] = json_encode($moduleData);
             $modArr = $moduleData->toArray();
@@ -62,9 +59,10 @@ class Iris extends ComponentBase
                 $filterObj = array_filter(
                     $modArr,
                     function ($e) use ($filter) {
-                        return $e['module_id'] === $filter;
+                        return intval($e['module_id']) === intval($filter);
                     }
                 );
+
                 $obj = array_shift($filterObj);
                 $finalData = $this->buildTree($modArr,$obj['parent_id'], $filter);
 
@@ -150,14 +148,21 @@ class Iris extends ComponentBase
     }
 
     private function buildTree(array &$elements, $parentId = 1, $moduleFilter=null) {
+echo "prent: ".$parentId."  --";
+echo "filter: ".$moduleFilter;
         $branch = array();
+        $i=0;
         foreach ($elements as $key=>$module) {
-
+echo $i."-";$i++;
             if($module['published'] == "1")//if not published don't include it
             {
                 if(!is_null($moduleFilter)&&($module['module_id']!=$moduleFilter))
                 {//if we have a filter and this module doesn't match the filter, skip the item
+
+                    echo "build tree -";
+                    echo count($elements)."|";
                     unset($elements[$module['module_id']]);
+                    echo count($elements);
                     continue;
                 }
                 if ($module['parent_id'] == $parentId) {
