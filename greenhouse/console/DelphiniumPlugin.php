064bd07295d6b1f13d2dbb065af199cd2d525e47
Business <Business>
2016-04-01 11:21:21 -0600

    WIP- Artisan command for vanilla plugin

--- greenhouse/console/DelphiniumPlugin.php
+++ greenhouse/console/DelphiniumPlugin.php
@@ -6,10 +6,12 @@ use System\Classes\UpdateManager;
 use System\Classes\PluginManager;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
+use Delphinium\Greenhouse\Templates\Plugin;
 
 class DelphiniumPlugin extends Command
 {
 
+    protected $fileMap = [];
     /**
      * The console command name.
      * @var string
@@ -37,21 +39,32 @@ class DelphiniumPlugin extends Command
      */
     public function fire()
     {
-//        $this->output->writeln('Hello world!');
-//        $this->info('Display this on the screen');
-//        $this->output->writeln(sprintf('<info>Unpacking plugin: %s</info>', $code));
         $name = explode(".", $this->argument('name'));
 //        $this->output->writeln(sprintf('<info>Argument given: %s</info>', $name[0]));
 //        $this->output->writeln(sprintf('<info>Argument given: %s</info>', $name[1]));
         //to call another command from this command:
 //        $this->call("create:plugin");
-        $this->call("create:plugin", ['pluginCode' => $this->argument('name')]);//here we create a new plugin.
+//        $this->call("create:plugin", ['pluginCode' => $this->argument('name')]);//here we create a new plugin.
+        $parts = explode('.', $this->argument('name'));
+
+        if (count($parts) != 2) {
+            $this->error('Invalid plugin name, either too many dots or not enough.');
+            $this->error('Example name: AuthorName.PluginName');
+            return;
+        }
+
+
+        $pluginName = array_pop($parts);
+        $authorName = array_pop($parts);
+        $vars = [
+            'name'   => $pluginName,
+            'author' => $authorName,
+        ];
+        $destinationPath = base_path() . '/plugins';
+        Plugin::make($destinationPath, $vars);
 
         //now let's try to create our own stub of what a Delphinium plugin should look like:
-        protected $fileMap = [
-        'plugin/plugin.stub'  => '{{lower_author}}/{{lower_name}}/Plugin.php',
-        'plugin/version.stub' => '{{lower_author}}/{{lower_name}}/updates/version.yaml',
-    ];
+
 
         //PluginManager $this->loadPlugin($namespace, $path);
         //UpdateManager->updatePlugin($name)
@@ -95,5 +108,46 @@ class DelphiniumPlugin extends Command
     protected function getOptions()
     {
         return [];
+    }
+
+    private function delphiniumMakeAll()
+    {
+        $stubs = array_keys($this->fileMap);
+        foreach ($stubs as $stub) {
+            $this->makeStub($stub);
+        }
+    }
+
+    private function makeStub($stubName)
+    {
+        if (!isset($this->fileMap[$stubName]))
+            return;
+
+        $sourceFile = __DIR__ . '/Templates/' . $stubName;
+        $destinationFile = $this->targetPath . '/' . $this->fileMap[$stubName];
+        $destinationContent = $this->files->get($sourceFile);
+
+        /*
+         * Parse each variable in to the desintation content and path
+         */
+        foreach ($this->vars as $key => $var) {
+            $destinationContent = str_replace('{{'.$key.'}}', $var, $destinationContent);
+            $destinationFile = str_replace('{{'.$key.'}}', $var, $destinationFile);
+        }
+
+        /*
+         * Destination directory must exist
+         */
+        $destinationDirectory = dirname($destinationFile);
+        if (!$this->files->exists($destinationDirectory))
+            $this->files->makeDirectory($destinationDirectory, 0777, true); // @todo 777 not supported everywhere
+
+        /*
+         * Make sure this file does not already exist
+         */
+        if ($this->files->exists($destinationFile) && !$this->overwriteFiles)
+            throw new \Exception('Stop everything!!! This file already exists: ' . $destinationFile);
+
+        $this->files->put($destinationFile, $destinationContent);
     }
 }
