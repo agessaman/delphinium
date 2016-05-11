2b6a3a0cecd92c4d2a7b8a54141ff2179ccdd6d8
Jacob Reid <reidwjacob@gmail.com>
2016-05-11 13:25:15 -0600

    Ordered by point value

--- blossom/assets/javascript/eastereggs.js
+++ blossom/assets/javascript/eastereggs.js
@@ -19,9 +19,21 @@
  * You may not distribute any version of this software, modified or otherwise
  */
 
-var eggs = ['harlem_shake','ripples','asteroids','katamari','bombs','ponies','my_little_pony','snow', 'raptor', 'fireworks'];
-var eggsIcons = ['bolt','bullseye','rocket','fa fa-soccer-ball-o','fa fa-bomb','linux','github-alt','gears','gears', 'gears'];
-var comands = ['Press "h" and "a" at the same time, make sure your sound is on', 'Press "r" and "i" at the same time and move the mouse', 'Press "a" and "s" at the same time, space to shoot, arrow keys to move', 'Press "k" and "a" at the same time, follow instructions', 'Press "b" and "o" at the same time, click mouse in text to drop them', 'Press "p" and "o" at the same time, space to re-spawn, arrow keys to move', 'Press "m" and "y" at the same time, watch and enjoy','Press "s" and "n" at the same time, watch and enjoy, follows mouse, break lights', 'Press "r" and "a" at the same time, make sure your sound is on', 'Press "f" and "i" at the same time, make sure your sound is on'];
+var eggs = [{ name:'harlem_shake', icon:'bolt', comand:'Press "h" and "a" at the same time, make sure your sound is on', value:config.harlem_shake},
+            { name:'ripples', icon:'bullseye', comand: 'Press "r" and "i" at the same time and move the mouse', value:config.ripples},
+            { name:'asteroids', icon:'rocket', comand: 'Press "a" and "s" at the same time, space to shoot, arrow keys to move', value:config.asteroids},
+            { name:'katamari', icon:'fa fa-soccer-ball-o', comand:'Press "k" and "a" at the same time, follow instructions', value:config.katamari}, 
+            { name:'bombs', icon:'fa fa-bomb', comand:'Press "b" and "o" at the same time, click mouse in text to drop them', value:config.bombs},
+            { name:'ponies', icon:'linux', comand:'Press "p" and "o" at the same time, space to re-spawn, arrow keys to move', value:config.ponies},
+            { name:'my_little_pony', icon:'github-alt', comand:'Press "m" and "y" at the same time, watch and enjoy', value:config.my_little_pony},
+            { name:'snow', icon:'gears', comand:'Press "s" and "n" at the same time, watch and enjoy, follows mouse, break lights', value:config.snow},
+            { name:'raptor', icon:'gears', comand:'Press "r" and "a" at the same time, make sure your sound is on', value:config.raptor},
+            { name:'fireworks', icon:'gears', comand:'Press "f" and "i" at the same time, make sure your sound is on', value:config.fireworks}];
+
+eggs.sort(function(a,b){
+  return a.value - b.value;
+});
+
 var keys = {};
 var count = 65;
 var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
