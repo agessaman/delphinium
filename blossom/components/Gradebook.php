82a4ac147f93b8e59902834a3a6649d53d863181
Vladimir Kirakosyan <v.kirakosyan84@gmail.com>
2016-07-04 09:25:51 +0400

    red line

--- blossom/components/Gradebook.php
+++ blossom/components/Gradebook.php
@@ -378,7 +378,7 @@ class Gradebook extends ComponentBase {
         $zeroMile->date = $experienceStartDateUTC->format('c');
         $newArr[] = $zeroMile;
 
-        $interval = "P0D";
+        $interval = "PT2H";
         $dayBeforeMile = new \stdClass();
         $dayBeforeMile->points =0;
         $dayB = $firstD->sub(new DateInterval($interval));
