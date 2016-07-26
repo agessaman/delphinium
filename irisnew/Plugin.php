292b7930d91e6550a91e507008227270b51d7423
Vladimir Kirakosyan <v.kirakosyan84@gmail.com>
2016-07-26 18:24:19 +0400

    irisnew chart

--- irisnew/Plugin.php
+++ irisnew/Plugin.php
@@ -7,7 +7,7 @@ class Plugin extends PluginBase
 {
     public function registerComponents()
     {
-        $componentArray = array('\\Delphinium\\Irisnew\\Components\\IrisWalkThrough' => 'iriswalkthrough');
+        $componentArray = array('\\Delphinium\\Irisnew\\Components\\IrisWalkThrough' => 'iriswalkthrough', '\\Delphinium\\Irisnew\\Components\\IrisChart' => 'irischart');
         return $componentArray;
     }
     public function registerSettings()
@@ -16,7 +16,21 @@ class Plugin extends PluginBase
     function boot()
     {
         \Event::listen('backend.menu.extendItems', function ($manager) {
-            $manager->addSideMenuItems('Delphinium.Greenhouse', 'greenhouse', array('WalkThrough' => array('label' => 'WalkThrough', 'icon' => 'oc-icon-adjust', 'owner' => 'Delphinium.Greenhouse', 'url' => \Backend::url('delphinium/irisnew/walkthrough'), 'group' => 'irisnew')));
+            $manager->addSideMenuItems('Delphinium.Greenhouse', 'greenhouse', array(
+                'WalkThrough' => array(
+                    'label' => 'WalkThrough', 
+                    'icon' => 'oc-icon-adjust', 
+                    'owner' => 'Delphinium.Greenhouse', 
+                    'url' => \Backend::url('delphinium/irisnew/walkthrough'), 
+                    'group' => 'irisnew'), 
+                'IrisChart' => array(
+                    'label' => 'IrisChart', 
+                    'icon' => 'oc-icon-pie-chart', 
+                    'owner' => 'Delphinium.Greenhouse', 
+                    'url' => \Backend::url('delphinium/irisnew/irischart'), 
+                    'group' => 'irisnew')
+                )
+            );
         });
     }
 }
